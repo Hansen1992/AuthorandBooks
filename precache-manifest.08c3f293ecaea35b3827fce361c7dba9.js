3c899545f3b465204a788663d668382ae5730391
Martin Nøhr Åskov Hansen <noehraaskov@gmail.com>
2019-10-20 17:00:00 +0200

    Updates

---
+++ precache-manifest.08c3f293ecaea35b3827fce361c7dba9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de597ddf59cf20d2c32e4b376902ccdc",
+    "url": "/AuthorsAndBooks/index.html"
+  },
+  {
+    "revision": "a2f5bd79484e798da353",
+    "url": "/AuthorsAndBooks/static/css/main.6f744c4c.chunk.css"
+  },
+  {
+    "revision": "6d8de9bd23d866534aaf",
+    "url": "/AuthorsAndBooks/static/js/2.16fd842a.chunk.js"
+  },
+  {
+    "revision": "a2f5bd79484e798da353",
+    "url": "/AuthorsAndBooks/static/js/main.30c4ad4b.chunk.js"
+  },
+  {
+    "revision": "8c05ac0fb1189422a8d0",
+    "url": "/AuthorsAndBooks/static/js/runtime-main.4eef00a3.js"
+  }
+]);
